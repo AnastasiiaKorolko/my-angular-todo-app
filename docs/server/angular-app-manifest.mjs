3e8b59717761966103070b9cafb7884c389dda64
Korolko Anastasiia <korolkonasta@gmail.com>
2025-04-09 14:52:18 +0300

    Update todo-list-component.html and rebuild project

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -17,9 +17,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 493, hash: '2667f5dffbd86e84d6e150a1fb40679a115b1904d658a1d55dfd9157baa51cc4', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1006, hash: 'ad683891577719b9f485588b7c7d16565b1c8ce8593fa24998fbbe1569c55ef8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 11796, hash: '6a7f96b1d35d96eb316ad6c59b4b97f9a121dcbd549ed00938b1057d0123d4b7', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 493, hash: '4e8c2b9588d89d6edd7977c366d83360aeeed786e42a4419fda3ed59f10b82c8', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1006, hash: '329f24a3ae74630bcdb6a9e41a60a4e3ad1d8c24251f15bb90b10614173eca01', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 11512, hash: '1acc3b183feebefec74029323cd0739b0e7da7035cb2332b90b84c95c63e119d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
--- docs/server/assets-chunks/index_csr_html.mjs
+++ docs/server/assets-chunks/index_csr_html.mjs
@@ -9,6 +9,6 @@ export default `<!doctype html>
 <link rel="stylesheet" href="styles-5INURTSO.css"></head>
 <body ngcm="">
   <app-root></app-root>
-<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-UB2KIL2O.js" type="module"></script></body>
+<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-HV66F25W.js" type="module"></script></body>
 </html>
 `;
